1700ddad72f73b48b29a0b4ee73ae6647b82d809
Kristian <kristian.heikinpoika@gmail.com>
2019-03-29 00:27:54 +0200

    Fix manifest script.

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -6,8 +6,8 @@ docker build . -f Dockerfile.x86 -t tyhjataulu/go-edge-api:x86
 docker push tyhjataulu/go-edge-api:arm
 docker push tyhjataulu/go-edge-api:x86
 
-docker manifest create --amend tyhjataulu/go-edge-api:latest \
+docker manifest create tyhjataulu/go-edge-api:latest \
 tyhjataulu/go-edge-api:arm \
 tyhjataulu/go-edge-api:x86
 
-docker manifest push tyhjataulu/go-edge-api:latest
+docker manifest push tyhjataulu/go-edge-api:latest -p
